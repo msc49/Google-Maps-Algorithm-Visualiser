7259e0e09dd7f3ea27c2b9fff6c339cde39b7415
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2022-02-01 19:25:39 +0000

    added rgb colour numbers for our nodes

---
+++ node.py
@@ -0,0 +1,15 @@
+from queue import PriorityQueue
+import pygame
+import math
+
+
+# lets add some constants which will represent the colours for our nodes using RGB colours
+ORANGE = (255, 165 ,0) # starting node
+BLUE = (0, 255, 0) # ending node
+BLACK = (0, 0, 0) # wall node
+TURQUOISE = (64, 224, 208) # path finder
+WHITE = (255, 255, 255) # default nodes
+
+
+
+
