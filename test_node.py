cb7999354018dca0f9b174295d6cbd8c5ba333aa
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2022-02-02 21:08:56 +0000

    green tests for node class

--- test_node.py
+++ test_node.py
@@ -1,2 +1,21 @@
 import pytest
 from node import Node, ORANGE, BLUE, TURQUOISE, BLACK, WHITE
+
+
+def test_default_node_colour():
+  node = Node(1, 1, 14, 50)
+  assert node.colour == WHITE
+
+def test_initialised_with_no_neighbours():
+    node = Node(1, 1, 14, 50)
+    assert node.neighbors == []
+
+
+def test_wall_can_be_made():
+  node = Node(1, 1, 14, 50)
+  node.make_wall()
+  assert node.colour == BLACK
+
+def test_get_node_position():
+    node = Node(1, 1, 14, 50)
+    assert node.get_node_position() == (1,1)
