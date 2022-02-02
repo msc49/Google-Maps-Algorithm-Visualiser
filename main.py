cd152ffc3a13eca12c9b7891000e595509b02063
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2022-02-02 03:38:40 +0000

    set up pygame display

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ from queue import PriorityQueue
 import pygame
 import math
 
+# In Queue, the oldest element is dequeued first. While, in Priority Queue, element based on highest priority is dequeued.
+pygame.display.set_caption('Google Maps Algorithm Visualiser (Dijkstras Algorithm)')
+WIDTH = 1000
+LENGTH = WIDTH
+WINDOW = pygame.display.set_mode((WIDTH,LENGTH))
